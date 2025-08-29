2839d6b19d7c08da9861919d50a26ca238a81e62
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-08-29 23:34:42 +0900

    chore: add site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,9 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kirinohanovel.com/natsuiro/',
   image: {
     responsiveStyles: true,
   },
-
   integrations: [react()],
 });
