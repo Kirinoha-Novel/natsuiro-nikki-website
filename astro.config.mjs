92c82af4e7870264f04b4b48c2ac78f7f859125c
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-11-28 06:06:20 +0900

    chore: update news config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   image: {
     responsiveStyles: true,
   },
-  integrations: [react()],
+  site: 'https://natsuiro.kirinohanovel.com',
+  integrations: [react(), sitemap()],
 });
