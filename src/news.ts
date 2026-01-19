aa04157bfb0315a95809b0ede39fa039fb1756c8
USP113 <hikarisanshi11@gmail.com>
2026-01-20 02:06:11 +0900

    お知らせ欄追加

--- src/news.ts
+++ src/news.ts
@@ -25,6 +25,21 @@ export const infoArray: Info[] = [
 
 export const infoArray: Info[] = [
   {
+    url: 'https://x.com/yuki_osada0808/status/2006063619889582124?s=20',
+    date: '2025年12月31日',
+    title: 'C107特設サイト公開',
+  },
+  {
+    url: 'https://x.com/kirinohanovel/status/1982765503753134549?s=20',
+    date: '2025年10月27日',
+    title: 'コミックつくば！特設サイト公開',
+  },
+  {
+    url: 'https://x.com/kirinohanovel/status/1976997276129390953?s=20',
+    date: '2025年10月11日',
+    title: 'キャラクター公開',
+  },
+  {
     url: 'https://x.com/kirinohanovel/status/1962108105124360515',
     date: '2025年8月31日',
     title: 'ティザーサイト公開',
